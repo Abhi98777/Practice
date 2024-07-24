c5826192f1cbc3bed6497082944c0eb71722e5b8
Abishek das <dasabishek335@gmail.com>
2024-07-24 16:24:21 +0545

    Table created and called using controller

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -87,7 +87,7 @@
                 <div class="links">
                     <a href="{{ route('first')}}">Name Route check</a>
                     <a href="{{url('/first')}}">url check</a>
-                    <a href="https://laravel-news.com">News</a>
+                    <a href="{{ route('Table')}}">TAble </a>
                     <a href="https://blog.laravel.com">Blog</a>
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
