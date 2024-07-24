85768cc1ba68f27e7e721b3c1419c76053fb5e36
Abishek das <dasabishek335@gmail.com>
2024-07-24 16:19:12 +0545

    Controller created and blade file created which have name table blade php

---
+++ resources/views/table.blade.php
@@ -0,0 +1,26 @@
+<html>
+
+<table  border="1">
+    <caption>bit class</caption>
+    <thead>
+        <tr>
+        <th> 1 </th>
+        <th> 2 </th>
+        <th> 3 </th>
+</tr>
+
+<tbody>
+<tr>
+        <td> abishek</td>
+        <td>  jayprakash</td>
+        <td>  krishna</td>
+</tr>
+</tbody>
+
+<tfoot>
+
+</tfoot>
+
+</table>
+
+</html>
